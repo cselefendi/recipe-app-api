675129961963c5b2729c66267c309ffb9eeb5c66
cselefendi <cselefendi@gmail.com>
2021-06-02 16:13:43 +0100

    email validation and error checking

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Creates and saves a new user """
-
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=email.lower(), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,31 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, test_email)
         self.assertTrue(user.check_password(test_password))
+
+    def test_new_user_email_normalized(self):
+        """ Test that email for new user will be converted to lowercase """
+
+        for test_username in ['teszteles1', 'Teszteles2',
+                              'teszTELES3', 'TESZTELES4']:
+            test_email = f"{test_username}@bszu.dev"
+            test_password = 'testpass@123'
+
+            user = get_user_model().objects.create_user(
+                email=test_email,
+                password=test_password
+            )
+
+            self.assertEqual(user.email, test_email.lower())
+            self.assertTrue(user.check_password(test_password))
+
+    def test_new_user_invalid_email(self):
+        """ Test that email without email cannot be created """
+
+        with self.assertRaises(ValueError):
+            test_email = None
+            test_password = 'testpass@123'
+
+            get_user_model().objects.create_user(
+                email=test_email,
+                password=test_password
+            )
